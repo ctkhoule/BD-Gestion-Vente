b686663e1b0a8db12044c3820fdc3c164d6c2bf8
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-03-17 20:08:11 +0000

    Insertion dans la table Utilisateur

--- insertion.sql
+++ insertion.sql
@@ -13,3 +13,17 @@ values
 ('B123ABXS', 'Nom entreprise ', 'www.exemple.com'),
 ('B123ABVB', 'Nom entreprise', 'www.exemple.net'),
 ('B123ABSW', 'Nom entreprise', 'www.exemple.com');
+
+-- Table Utilisateur
+INSERT INTO Utilisateur (id_utilisateur, nom_utilisateur)
+values
+('user120', 'Nom utilisateur'),
+('user121', 'Nom utilisateur'),
+('user122', 'Nom utilisateur'),
+('user123', 'Nom utilisateur'),
+('user124', 'Nom utilisateur'),
+('user125', 'Nom utilisateur'),
+('user126', 'Nom utilisateur'),
+('user127', 'Nom utilisateur'),
+('user128', 'Nom utilisateur'),
+('user129', 'Nom utilisateur');
