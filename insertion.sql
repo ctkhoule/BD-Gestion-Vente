556f7ef2a9fea1a0f7aa619bae73c9b70962fcd7
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-03-17 20:05:58 +0000

    Insertion dans la table Entreprise

---
+++ insertion.sql
@@ -0,0 +1,15 @@
+-- Insertion
+
+-- Table Entreprise
+INSERT INTO Entreprise (id_entreprise, nom_entreprise, url_entreprise)
+values
+('B123ABCD', 'Nom entreprise', 'www.exemple.sn'),
+('B123ABEG', 'Nom entreprise', 'www.exemple.sn'),
+('B123ABAO', 'Nom entreprise', 'www.exemple.com'),
+('B123ABDK', 'Nom entreprise', 'www.exemple.net'),
+('B123ABKD', 'Nom entreprise', 'www.exemple.sn'),
+('B123ABPO', 'Nom entreprise', 'www.exemple.com'),
+('B123ABVD', 'Nom entreprise', 'www.exemple.sn'),
+('B123ABXS', 'Nom entreprise ', 'www.exemple.com'),
+('B123ABVB', 'Nom entreprise', 'www.exemple.net'),
+('B123ABSW', 'Nom entreprise', 'www.exemple.com');
