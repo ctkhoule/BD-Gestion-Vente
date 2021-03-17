74ed67e8540636931ba2e9146ea598340ccddf44
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-03-17 20:01:14 +0000

    Ajout table Vente

--- tables.sql
+++ tables.sql
@@ -19,3 +19,18 @@ Create Table Produit (
     titre_produit varchar(15),
     description_produit varchar(100)
 );
+
+-- Table Vente
+Create Table Vente (
+    id_utilisateur varchar(15),
+    id_produit varchar(15),
+    id_entreprise varchar(15),
+    dateCreation date,
+    reference varchar(15),
+    prix integer,
+    quantite integer,
+   	CONSTRAINT pk_vente PRIMARY KEY (id_utilisateur, id_produit, id_entreprise, dateCreation),
+    CONSTRAINT fk_vente_entreprise FOREIGN KEY (id_entreprise) REFERENCES Entreprise(id_entreprise),
+    CONSTRAINT fk_vente_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur),
+    CONSTRAINT fk_vente_produit FOREIGN KEY (id_produit) REFERENCES Produit(id_produit)
+);
