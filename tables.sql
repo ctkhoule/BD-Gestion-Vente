9b94bb6d6376abd556e99c0be5134785d12dc960
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-03-17 19:55:59 +0000

    Ajout table Entreprise

---
+++ tables.sql
@@ -0,0 +1,10 @@
+-- Creation des tables
+
+-- Table Entreprise
+Create Table Entreprise (
+    id_entreprise varchar(15) primary key,
+    nom_entreprise varchar(20),
+    url_entreprise text
+);
+
+
